3d6b7fb3e7e3efcb09c48fec44688fc57a16188b
Ilya <86732040+Gitsans66@users.noreply.github.com>
2021-11-12 22:32:44 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+
+setup(
+    name="FuncPy",
+    version="0.0.1",
+    author="GitNov66 company",
+    author_email="novaus65@gmail.com",
+    description='''s library is a collection of activation functions for a neural network.
+This library contains the following functions
+binary_step
+sigmoid
+tanf
+ReLU
+softplus
+leaky_ReLU''',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FuncPy/homepage",
+    packages=find_packages(),
+    install_requires=requirements_dev,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: BSD 3-Clause License",
+    ],
+)
